d51c7836887f72f9a8286de0d6e0a41c17e8f560
dasheq <dszczepny@gmail.com>
2015-09-30 14:53:58 +0000

    Displaying the list

--- lab3/palindrome.py
+++ lab3/palindrome.py
@@ -1,6 +1,8 @@
 #Program to check if a word is a palindrome
-word1 = Oxo
-word2 = OXO
-word3 = 123454321
-word4 = ROTATOR
-word5 = 12345 54321
+
+words =['Oxo','OXO','123454321','ROTATOR','12345 54321']
+words2 =[]
+
+for i in words:
+ # words2[i] = reversed(words[i])
+  print "%s" % i
