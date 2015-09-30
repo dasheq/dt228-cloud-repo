3ec3801aaa0f529f2a08deb0ad38ab5fb85a73cf
dasheq <dszczepny@gmail.com>
2015-09-30 15:42:11 +0000

    Palindrome working + Comments

--- lab3/palindrome.py
+++ lab3/palindrome.py
@@ -1,25 +1,31 @@
-#Program to check if a word is a palindrome
+def is_palindrome(value):
+    min = 0
+    max = len(value) - 1
 
-words =['Oxo','OXO','123454321','ROTATOR','12345 54321']
+    #This program compares the first and last letter of the word 
+    #If they're equal, move onto next letter until after passing the middle 
+    while True:
 
-for i in words:
- # words2[i] = reversed(words[i])
-  words2[i] = reversed(words[i])
-  print "%s" % i
-  
-def is_palindrome(words):
-  min = 0
-  max = len(words) - 1
-  
-  while True:
-    if min > max:
-      return True
-    
-    a = value[min]
-    b = value[max]
+        #If all letters checked return true
+        if min > max:
+            return True;
 
-    if a != b:
-      return False;
+        a = value[min]
+        b = value[max]
 
-      min += 1
-      max -= 1
+        #Return false is characters are not equal.
+        if a != b:
+            return False;
+
+        #Move to next letter
+        min += 1
+        max -= 1
+
+words = ["Oxo","OXO","123454321","ROTATOR","12345 54321"]
+
+for word in words:
+
+    if is_palindrome(word):
+        print("True: ", word)
+    else:
+        print("False: ", word)
